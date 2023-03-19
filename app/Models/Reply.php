9133b7ae6bb327fe16a931d3148a97c96a4f0efc
dewanakl <dewanakretarta29@gmail.com>
2023-03-19 20:23:27 +0700

    feat: add models

---
+++ app/Models/Reply.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model\Model;
+
+final class Reply extends Model
+{
+    protected $table = 'replies';
+}
---
+++ app/Models/Room.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model\Model;
+
+final class Room extends Model
+{
+    protected $table = 'rooms';
+}
